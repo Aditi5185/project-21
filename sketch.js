512f4b381c4a77cfac7e7b1592d913465679b37a
Aditi5185 <88102819+Aditi5185@users.noreply.github.com>
2022-03-06 12:48:32 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,42 @@
+var ball
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+var radius = 40
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(1600, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+var options = {
+	isStatic:false,
+	restitution:1,friction:0,density:1.2
+	
+}
+ball = Bodies.circle(260,100,radius,options)
+	World.add(world,ball)
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  ellipse(ball.position.x,ball.position.y,radius,radius)
+  groundObj.display()
+  groundObj= new ground(width/2,670,width,20);
+  leftSide = new ground(1100,600,20,120);
+  drawSprites();
+ 
+}
+
